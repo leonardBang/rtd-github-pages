04bf640874f57f437725d4147c9ac83bc80d3de4
Leonard Xu <xbjtdcq@gmail.com>
2021-08-08 17:42:36 +0800

    test version

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -68,10 +68,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# multiple version supports
-import sys, os
-sys.path.insert(0, os.path.abspath('../src'))
 
 try:
    html_context
